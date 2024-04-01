91078db7e768dc132cfff822299db374fce8fc95
Yury Nevalenny <Yury.Nevalenny@microsoft.com>
2024-04-01 18:02:19 +0000

    try to fix no thread message

--- backend/app/telegram/message_processors.go
+++ backend/app/telegram/message_processors.go
@@ -445,14 +445,16 @@ func ChunkSendMessage(bot *telego.Bot, message *telego.Message, text string) {
 	}
 	chatID := message.Chat.ChatID()
 	for _, chunk := range util.ChunkString(text, 4000) {
-		var err error
-		if message.MessageThreadID != 0 {
-			_, err = bot.SendMessage(tu.Message(chatID, chunk).WithMessageThreadID(message.MessageThreadID).WithReplyMarkup(getLikeDislikeReplyMarkup(message.MessageThreadID)))
-		} else {
-			_, err = bot.SendMessage(tu.Message(chatID, chunk).WithReplyMarkup(getLikeDislikeReplyMarkup(message.MessageThreadID)))
-		}
+		_, err := bot.SendMessage(tu.Message(chatID, chunk).WithMessageThreadID(message.MessageThreadID).WithReplyMarkup(getLikeDislikeReplyMarkup(message.MessageThreadID)))
 		if err != nil {
-			log.Errorf("Failed to send message to telegram: %v, chatID: %s, threadID: %d", err, chatID, message.MessageThreadID)
+			if strings.Contains(err.Error(), "message thread not found") {
+				// retry without message thread id
+				_, err = bot.SendMessage(tu.Message(chatID, chunk).WithReplyMarkup(getLikeDislikeReplyMarkup(message.MessageThreadID)))
+			}
+
+			if err != nil {
+				log.Errorf("Failed to send message to telegram: %v, chatID: %s, threadID: %d", err, chatID, message.MessageThreadID)
+			}
 		}
 	}
 }
