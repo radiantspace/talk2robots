4d558591735a074a0d3a07af11e8a6b088e6cf33
Yury Nevalenny <Yury.Nevalenny@microsoft.com>
2024-05-13 08:41:02 +0000

    only to private channels

--- backend/app/telegram/telegram.go
+++ backend/app/telegram/telegram.go
@@ -209,7 +209,9 @@ func handleMessage(bot *telego.Bot, message telego.Message) {
 			if !strings.HasPrefix(message.Document.MimeType, "audio/") && !strings.HasPrefix(message.Document.MimeType, "video/") {
 				log.Warnf("Ignoring non-audio document message in chat %s, mimetype: %s", chatIDString, message.Document.MimeType)
 
-				bot.SendMessage(tu.Message(chatID, "I don't support non-audio documents yet, stay tuned for updates.").WithMessageThreadID(message.MessageThreadID))
+				if isPrivate {
+					bot.SendMessage(tu.Message(chatID, "I don't support non-audio documents yet, stay tuned for updates.").WithMessageThreadID(message.MessageThreadID))
+				}
 				return
 			}
 		}
