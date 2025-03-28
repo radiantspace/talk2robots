42a9a6030e338d5152baba9fb45e69ce21f37a38
Yury Nevalenny <radiantspace@github.com>
2025-03-28 17:58:18 +0300

    fix fireworks models

--- backend/app/models/fireworks.go
+++ backend/app/models/fireworks.go
@@ -3,7 +3,7 @@ package models
 const (
 	// Fireworks
 	Firellava_13b Engine = "accounts/fireworks/models/firellava-13b"
-	LlamaV3_70b   Engine = "accounts/fireworks/models/llama-v3-70b-instruct"
-	LlamaV3_8b    Engine = "accounts/fireworks/models/llama-v3-8b-instruct"
+	LlamaV3_70b   Engine = "accounts/fireworks/models/llama-v3p3-70b-instruct"
+	LlamaV3_8b    Engine = "accounts/fireworks/models/llama-v3p2-11b-vision-instruct"
 	Llava_yi_34b  Engine = "accounts/fireworks/models/llava-yi-34b"
 )
