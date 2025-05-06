fd5837f4fa48c917520b5c8070a7f1a2dcbd53af
Yury Nevalenny <radiantspace@github.com>
2025-05-06 18:23:47 +0300

    Update fireworks.go

--- backend/app/models/fireworks.go
+++ backend/app/models/fireworks.go
@@ -4,6 +4,6 @@ const (
 	// Fireworks
 	Firellava_13b Engine = "accounts/fireworks/models/firellava-13b"
 	LlamaV3_70b   Engine = "accounts/fireworks/models/llama-v3p3-70b-instruct"
-	LlamaV3_8b    Engine = "accounts/fireworks/models/llama-v3p2-11b-vision-instruct"
+	LlamaV3_8b    Engine = "accounts/fireworks/models/llama-v3p1-8b-instruct"
 	Llava_yi_34b  Engine = "accounts/fireworks/models/llava-yi-34b"
 )
