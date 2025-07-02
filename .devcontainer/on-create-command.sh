78bc37b2586e45a35373a87078dd6f8e7f017815
Yury Nevalenny <radiantspace@github.com>
2025-07-02 07:37:32 +0000

    fix

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -2,5 +2,6 @@
 if [ -n "${CODESPACES:-}" ]; then
   sudo apt-get install pkg-config libogg-dev libvorbis-dev libopus-dev libopusfile-dev
   cd /workspaces/talk2robots
+  touch .env
   make tidy
 fi
